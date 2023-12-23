6b2c45eff68f9955bbde1272a71cdc98a120f1f2
Muhammad-Qzih <a90235145+1@gmail.com>
2023-12-23 21:35:05 +0200

    make the creation of services not costly and to improve the performance by create class Services this is  something like Singleton design pattern , and create interface for DataCollector to avoid dependency inversion

---
+++ src/edu/najah/cap/data/Services.java
@@ -0,0 +1,46 @@
+package edu.najah.cap.data;
+
+import edu.najah.cap.activity.IUserActivityService;
+import edu.najah.cap.activity.UserActivityService;
+import edu.najah.cap.iam.IUserService;
+import edu.najah.cap.iam.UserService;
+import edu.najah.cap.payment.IPayment;
+import edu.najah.cap.payment.PaymentService;
+import edu.najah.cap.posts.IPostService;
+import edu.najah.cap.posts.PostService;
+
+public class Services {
+    private static IUserService userService;
+    private static IUserActivityService userActivityService;
+    private static IPostService postService;
+    private static IPayment paymentService;
+
+    public static synchronized IUserService getUserServiceInstance() {
+        if (userService == null) {
+            userService = new UserService();
+
+        }
+        return userService;
+    }
+
+    public static synchronized IUserActivityService getUserActivityServiceInstance() {
+        if (userActivityService == null) {
+            userActivityService = new UserActivityService();
+        }
+        return userActivityService;
+    }
+
+    public static synchronized IPostService getUserPostServiceInstance() {
+        if (postService == null) {
+            postService = new PostService();
+        }
+        return postService;
+    }
+
+    public static synchronized IPayment getUserPaymentServiceInstance() {
+        if (paymentService == null) {
+            paymentService = new PaymentService();
+        }
+        return paymentService;
+    }
+}
